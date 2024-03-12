2558e2c143209270b7facfa98bd4f2a28cde9982
monosniper <monosniper@gmail.com>
2024-03-13 01:00:00 +0500

    sadas

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const corsOptions = {
     exposedHeaders: ['Content-Range']
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
