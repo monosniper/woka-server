5b0455023a7656e601cb7412c657a45c2768c88e
monosniper <monosniper@gmail.com>
2024-03-13 01:11:10 +0500

    sadas

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 // app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
