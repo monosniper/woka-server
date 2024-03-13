4f976087bb2ed15e4dbb9e2cac8dac9f9242b577
monosniper <monosniper@gmail.com>
2024-03-13 20:03:03 +0500

    dasd

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const corsOptions = {
 
 // app.use(cors(corsOptions));
 // app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next();
+// });
 app.use(logger('dev'));
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
