9d150d6e98786c0db8bc0d5f10541290457120e9
monosniper <monosniper@gmail.com>
2024-03-13 20:10:04 +0500

    dasd

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ require('dotenv').config()
 // app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Expose-Headers", "X-Total-Count");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "X-Total-Count,Content-Range");
     next();
 });
 app.use(logger('dev'));
