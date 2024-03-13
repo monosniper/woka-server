3371f0ab3a1baa11d562e270e0e7b8c312dbab8c
monosniper <monosniper@gmail.com>
2024-03-13 20:28:10 +0500

    dasd

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ require('dotenv').config()
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Expose-Headers", "X-Total-Count,Content-Range");
     next();
 });
