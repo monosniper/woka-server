5d1ead7066582ec53c627932aca7123a2f76100f
monosniper <monosniper@gmail.com>
2024-03-14 20:13:13 +0500

    dasd

--- get-online.js
+++ get-online.js
@@ -6,6 +6,10 @@ util.queryBasic('202.181.188.208', 25565, options)
     .then((result) => {
         fetch("http://localhost:5000/api/history", {
             method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
                 online: result.players.online
             })
