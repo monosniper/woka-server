a504401092498de85bed14d3453e2ac0a97c82f4
monosniper <monosniper@gmail.com>
2024-03-18 04:14:38 +0500

    asdsa

--- get-online.js
+++ get-online.js
@@ -1,24 +1,27 @@
 require('dotenv').config()
 const util = require('minecraft-server-util');
+const cron = require('node-cron');
 
 const options = {enableSRV: true};
 
-util.queryBasic(process.env.RCON_HOST, +process.env.QUERY_PORT, options)
-    .then((result) => {
-        fetch("http://localhost:5000/api/history", {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                online: result.players.online
+cron.schedule('* * * * *', function() {
+    util.queryBasic(process.env.RCON_HOST, +process.env.QUERY_PORT, options)
+        .then((result) => {
+            fetch("http://localhost:5000/api/history", {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    online: result.players.online
+                })
+            }).then(rs => {
+                console.log("Saved: " + result.players.online)
             })
-        }).then(rs => {
-            console.log("Saved: " + result.players.online)
         })
-    })
-    .catch((error) => console.error(error));
+        .catch((error) => console.error(error));
+});
 
 // const hosts = {
 //     'ANARCHY-M': {
