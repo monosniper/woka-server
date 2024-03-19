abc94c6a8ecd5aa2755e6d23bdf9b0cfb8acdba2
monosniper <monosniper@gmail.com>
2024-03-20 00:29:01 +0500

    asdasd

---
+++ _check-payments.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const cron = require('node-cron');
+const BuyService = require("./services/BuyService");
+const RCONService = require("./services/RCONService");
+const crypto = require("crypto");
+
+fetch("http://localhost:5000/api/buys?isCompleted=false")
+    .then(rs => rs.json())
+    .then(orders => {
+        orders.forEach(({id: orderId}) => {
+            const body = {
+                shopId: process.env.LAVA_SHOP_ID,
+                orderId,
+            }
+            const signature = crypto.createHmac("sha256", process.env.LAVA_SECRET_KEY)
+                .update(JSON.stringify(body))
+                .digest("hex");
+
+            fetch("https://api.lava.ru/business/invoice/status", {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Signature': signature
+                },
+                body: JSON.stringify(body)
+            })
+                .then(rs => rs.json())
+                .then(async rs => {
+                    if(rs.status === 200) {
+                        if(rs.data.status === 'success') {
+                            console.log("Order ID: " + orderId + " - Status - success")
+                            console.log("Run RCON commands")
+                            const buy = await BuyService.getOne(orderId)
+                            await buy.update({isCompleted: true})
+                            await RCONService.process(buy.name, buy.Products)
+                        }
+                    }
+                })
+        })
+    })
+
--- check-payments.js
+++ check-payments.js
@@ -41,3 +41,4 @@ cron.schedule('* * * * *', function() {
             })
         })
 });
+
