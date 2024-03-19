13491c7bdb1fe3657bb06b00b12cc5b9ee0e2fa9
monosniper <monosniper@gmail.com>
2024-03-20 00:25:51 +0500

    asdasd

--- check-payments.js
+++ check-payments.js
@@ -4,7 +4,7 @@ const BuyService = require("./services/BuyService");
 const RCONService = require("./services/RCONService");
 const crypto = require("crypto");
 
-cron.schedule('*/5 * * * *', function() {
+cron.schedule('* * * * *', function() {
     fetch("http://localhost:5000/api/buys?isCompleted=false")
         .then(rs => rs.json())
         .then(orders => {
