5558b274086d53f596c8611d467ab66ea818da36
monosniper <monosniper@gmail.com>
2024-03-21 18:56:23 +0500

    dasda

--- get-online.js
+++ get-online.js
@@ -3,24 +3,39 @@ const util = require('minecraft-server-util');
 const cron = require('node-cron');
 
 const options = {enableSRV: true};
+const url = "https://api.mcstatus.io/v2/status/java/193.164.18.13:25565"
 
 cron.schedule('* * * * *', function() {
-    util.queryBasic(process.env.RCON_HOST, +process.env.QUERY_PORT, options)
-        .then((result) => {
-            fetch("http://localhost:5000/api/history", {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    online: result.players.online
-                })
-            }).then(rs => {
-                console.log("Saved: " + result.players.online)
+    fetch(url).then(rs => rs.json()).then(({ players }) => {
+        fetch("http://localhost:5000/api/history", {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                online: players.online
             })
+        }).then(rs => {
+            console.log("Saved: " + players.online)
         })
-        .catch((error) => console.error(error));
+    })
+    // util.queryBasic(process.env.RCON_HOST, +process.env.QUERY_PORT, options)
+    //     .then((result) => {
+    //         fetch("http://localhost:5000/api/history", {
+    //             method: 'post',
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify({
+    //                 online: result.players.online
+    //             })
+    //         }).then(rs => {
+    //             console.log("Saved: " + result.players.online)
+    //         })
+    //     })
+    //     .catch((error) => console.error(error));
 });
 
 // const hosts = {
