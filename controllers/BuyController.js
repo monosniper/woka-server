386ad0e1aee638dd98e4e4e67b22c78254bca668
monosniper <monosniper@gmail.com>
2024-03-23 14:12:08 +0500

    asdas

--- controllers/BuyController.js
+++ controllers/BuyController.js
@@ -142,8 +142,7 @@ class BuyController {
             } else if (variant === 'aaio') {
                 try {
                     const currency = 'RUB'
-                    // const orderId = buy.id + '_' + Math.random().toString(16).slice(2)
-                    const orderId = buy.id
+                    const orderId = buy.id + '_' + uuidv4()
 
                     const body = {
                         amount,
